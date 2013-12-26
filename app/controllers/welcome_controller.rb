d8a2fa443f317df50d4fdeab3b32c9e17738400d
Eli Fatsi <eli.fatsi@viget.com>
2013-12-26 10:25:51 -0700

    Updated Core credentials and Dropcam

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,6 +7,7 @@ class WelcomeController < ApplicationController
 
   def click
     @status = Status.first
+
     if @status.status == "on"
       turn_off
       new_direction = "ON"
@@ -18,26 +19,18 @@ class WelcomeController < ApplicationController
     render json: {:new_direction => new_direction}
   end
 
-  def turn_off
-    @status.update_attribute(:status, "off")
-
-    params = access_param.merge(:params => "D7,LOW")
-    HTTParty.post(url, :body => params)
-  end
-
   def turn_on
     @status.update_attribute(:status, "on")
-
-    params = access_param.merge(:params => "D7,HIGH")
-    HTTParty.post(url, :body => params)
+    core.digital_write(7, "HIGH")
   end
 
-  def url
-    "https://api.spark.io/v1/devices/53ff74065067544828440487/digitalwrite"
+  def turn_off
+    @status.update_attribute(:status, "off")
+    core.digital_write(7, "LOW")
   end
 
-  def access_param
-    {:access_token => "81160ce514b1cc00cb24d8eccf47fabf336607b9"}
+  def core
+    @core ||= RubySpark::Core.new("53ff74065067544828440487")
   end
 
 end
---
+++ config/initializers/ruby_spark.rb
@@ -0,0 +1,3 @@
+RubySpark.configuration do |config|
+  config.access_token = "e5bdf3646a21af4c30b40c2c8d751b4fc83e7e32"
+end
